19ecbeefc2dff0f5d7815f9094f070c9eb49424a
zizominmjZI <gjduc3638.dg@gmail.com>
2024-12-16 22:44:51 +0200

    Add files via upload

---
+++ Kj&6cR3@5cLpig3H2^script.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bemka Game Description</title>
+    <link rel="stylesheet" href="stylesKj&6cR3@5cLpig3H2^.css">
+</head>
+<body>
+    <header>
+        <h1>Bemka</h1>
+    </header>
+    <main class="description">
+        <h2>وصف اللعبة</h2>
+        <p>
+            عنوان اللعبة: Bemka<br>
+            وصف اللعبة: انطلق في رحلة مثيرة مع "مغامرة المركب"، لعبة ثلاثية الأبعاد تجمع بين التحدي والإثارة. 
+            في هذه اللعبة، تتحكم في مركب يتحرك تلقائيًا عبر عالم ملون مليء بالدوائر، وعليك استخدام مهاراتك في التوجيه لتوجيه المركب بشكل صحيح.
+        </p>
+        <h3>روابط التحميل:</h3>
+        <ul>
+            <li><a href="https://drive.google.com/file/d/12l7VJwYw5RYu0ZIgyDu0tneTZ9LPv40_/view?usp=drive_link" target="_blank">تحميل اللعبة للأندرويد</a></li>
+            <li><a href="https://drive.google.com/file/d/17cs6KJ69z1gmCpqUZ4s-HSrq6Oew_bhv/view?usp=drive_link" target="_blank">تحميل اللعبة للويندوز</a></li>
+        </ul>
+    </main>
+</body>
+</html>
